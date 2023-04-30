c67e6a8806ef77f7a9e7dfb112ef3ebee5554efd
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-04-30 22:36:04 +0300

    lessons from 01to09 // 5

--- js/main.js
+++ js/main.js
@@ -1,16 +1,1 @@
-console.group("group 1");
-console.log("message one");
-console.log("message two");
-    console.groupCollapsed("child group");
-    console.log("message one");
-    console.log("message two");
-        console.groupCollapsed("grand child group");
-        console.log("message one");
-        console.log("message two");
-        console.groupEnd();
-    console.groupEnd();
-console.groupEnd();
-
-console.group("group 2");
-console.log("message one");
-console.log("message two");
+console.table(["ahmed", "ibrahem", "abdelaal", "hagag"]);
