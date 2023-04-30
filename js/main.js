8110fa043fc86056e99acbec5324315cd3f39eb2
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-04-30 22:19:28 +0300

    lessons from 01to09 // 3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-document.write("<h1>Elzero</h1>")
-document.querySelector("h1").style.fontFamily = "arial"
-document.querySelector("h1").style.color = "blue"
-document.querySelector("h1").style.fontSize = "80px"
-document.querySelector("h1").style.fontWeight = "bold"
-document.querySelector("h1").style.textAlign = "center"
+console.log ("%celzero %cweb %cschool",
+                "color:red; font-size:60px",
+                "color:green; font-size:60px; font-weight:bold;",
+                "background-color:blue; font-size:60px; color:white;");
