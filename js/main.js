98fa05db5e7544b9ed6849379bc8213467658626
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-01 23:07:13 +0300

    lessons10-17/2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,2 @@
-// declare variable
-let numberOne = 10;
-let numberTwo = 20;
-
-// print normal concatenate variables 
-console.log(numberOne + "" + numberTwo);
-
-// type of normal concatenate variables
-console.log(typeof (numberOne + "" + numberTwo));
-
-//print Template Literals concatenate variables 
-console.log(`${numberOne}${numberTwo}`);
-
-// type of Template Literals concatenate variables 
-console.log(typeof `${numberOne}${numberTwo}`);
-
-// print normal concatenate variables with new line
-console.log(numberTwo + "\n" + numberOne);
-
-//print Template Literals concatenate variables  with new line
-console.log(`${numberTwo} 
-${numberOne}`);
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
