1c8a9ff10fe78f82835e7a31d710b0ceaeadfbe7
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-01 23:31:33 +0300

    lessons10-17/4(final)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-let a = 21;
-let b = 20;
-
-console.log(`_${a}_${b}${a}_${b}${a}_${b}${a}_${b}_`); // _21_2021_2021_2021_20_
+ let a = 21;
+ let b = 20;
+ let con = "_" + b + "" + a + "_"
+let rep = con.repeat(3)
+console.log(`_${a}_${rep}_${b}_`); // _21_2021_2021_2021_20_
