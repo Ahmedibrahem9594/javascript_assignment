c06118ee1098b3bfd917dbe7cb508e0e5538dbd9
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-04 00:44:39 +0300

    lessons 27 to 30 \ 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log(Math.floor(Math.random() * 4)); // 0 || 1 || 2 || 3 || 4
+let userName = "Elzero";
+console.log(userName.charAt(0).toLowerCase()); // e
+console.log(userName.slice(0,1).toLowerCase()); // e
+console.log(userName.split("")[0].toLowerCase()); // e
+console.log(userName.substring(0,1).toLowerCase()); // e
+console.log(userName.slice(-userName.length,1).toLowerCase()); // e
+console.log(userName.substring(-userName.length,1).toLowerCase().repeat(3)); // eee
