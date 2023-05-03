d95c486334d9f36ece9de105d94b11f765b1bb5c
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-03 22:50:40 +0300

    Play With Variable Values challenge

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
-let points = 10;
-
-// Write Your Code Here
-++points
-++points
-++points
-console.log(points); // 13
-
-// Write Your Code Here
---points
---points
---points
---points
---points
-console.log(points); // 8;
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+console.log(-d * +e); // 2000
+console.log(++e * ++g + -d + ++f); // 173
