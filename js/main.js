f69baac49f26a9871a4e42486cba9acbe578eed2
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-03 23:48:43 +0300

    lessons 23 to 26 /1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-let d = "-100";
-let e = "20";
-let f = 30;
-let g = true;
-console.log(-d * +e); // 2000
-console.log(++e * ++g + -d + ++f); // 173
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(1e5); // 100000
+console.log(1e1**5); // 100000
+console.log(1e3 * 100 ); // 100000
+console.log(10000 + 90000); // 100000
+console.log(1000000/10); // 100000
+console.log(100000^0); // 100000
+console.log(100&&100000); // 100000
+console.log(200000-100000); // 100000
+console.log(Math.pow(10,5)); // 100000
+console.log(Math.max(100000)); // 100000
