b2a2833f31ae99218df0e644cbca39184100d3d2
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-04 00:01:41 +0300

    lessons 23 to 26 /2,3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
-// Examples
-console.log(100_000); // 100000
-console.log(100000); // 100000
-console.log(5e4 + 5e4); // 100000
-
-// Your Solutions
-console.log(1e5); // 100000
-console.log(1e1**5); // 100000
-console.log(1e3 * 100 ); // 100000
-console.log(10000 + 90000); // 100000
-console.log(1000000/10); // 100000
-console.log(100000^0); // 100000
-console.log(100&&100000); // 100000
-console.log(200000-100000); // 100000
-console.log(Math.pow(10,5)); // 100000
-console.log(Math.max(100000)); // 100000
+console.log(-Number.MIN_SAFE_INTEGER); // 9007199254740991
+console.log(Number.MAX_SAFE_INTEGER + Number.MIN_SAFE_INTEGER + 
+true + 
+true + 
+true + 
+true + 
+true + 
+true + 
+true + 
+true + 
+true + 
+true + 
+true + 
+true + 
+true +
+true +
+true +
+true
+); // 16
