33d72cfb31a615503017dd6bc41dfd22fbffe194
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-05 20:53:33 +0300

    lessons 33 - 37 /2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-// Test Case 1
-let num = 9; // "009"
-// Test Case 2
-let num = 20; // "020"
-// Test Case 3
-let num = 110; // "110"
-
-if (num < 10) {
-    console.log(`00${num}`);
-} else if (num > 10 && num < 100) {
-    console.log(`0${num}`);
-} else if (num >= 100) {
-    console.log(`${num}`);
+let num1 = 9;
+let str = "9";
+let str2 = "20";
+if (num1 == str) {
+    console.log(`${num1} Is The Same Value As ${str}`);
 }
+else if (num1 !== str) {
+    console.log(`${num1} Is The Same Value As ${str} But Not The Same Type`);
+}
+else if (num1 != str2) {
+    console.log(`${num1} Is Not The Same Value Or The Same Type As ${str2}`);
+}
+else if (typeof st == typeof str2 && str != str2) {
+    console.log(`${str} Is The Same Type As ${str2} But Not The Same Value`);
+}
+// Output
+// "{num1} Is The Same Value As {str}"
+// "{num1} Is The Same Value As {str} But Not The Same Type"
+// "{num1} Is Not The Same Value Or The Same Type As {str2}"
+// "{str} Is The Same Type As {str2} But Not The Same Value"
