d77591c8304da93058cd39815a3607e21b997177
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-05 20:27:23 +0300

    lessons 31-32 / 1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
-let a = 100;
-let b = 2_00.5;
-let c = 1e2;
-let d = 2.4;
-// Find Smallest Number In All Variables And Return Integer
-console.log(Math.min(a, b, c, d)); // 2
-// Use Variables a + d One Time To Get The Needed Output
-console.log( (a * Math.trunc(b)) / 2); // 10000
-// Get Integer "2" From d Variable With 4 Methods
-console.log(Math.trunc(d)); // 2
-console.log(Math.round(d)); // 2
-console.log(Math.floor(d)); // 2
-console.log(Math.ceil(d) - 1); // 2
-// Use Variables b + d To Get This Valus
-console.log((( Math.sqrt(b/d) + 57.53 ).toString()).substring(0,5) ); // 66.67 => String
-console.log(Math.round((( Math.sqrt(b/d) + 57.53 ).toString()).substring(0,5))); // 67 => Number
+console.log(100 == "100"); // true
+console.log(100 < 1000); // true
+console.log(110 > 100 > 10 < 20); // true
+console.log(-10 == "-10"); // true
+console.log(10 -50 == +"-40"); // true
+console.log(+ 10 != -"-40"); // true
+console.log(+ "10" == 10); // true
+console.log(+ 20 != false); // true
