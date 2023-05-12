d5aa1f56a572b81e98fc75f7f281010e5e2342fe
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-12 18:25:37 +0300

    Array Methods Challenge

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-let arr1 = ["A", "C", "X"];
-let arr2 = ["D", "E", "F", "Y"];
-let allArrs = [];
-
-// Your Code Here
-let xx = arr1.pop();
-let yy = arr2.pop();
-let ff = arr2.pop();
-allArrs = xx.toLowerCase()+ff.toLowerCase()+yy.toLowerCase();
-console.log(allArrs); // fxy
+let zero = 0;
+let counter = 3;
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
+//solution one
+my.pop() ; my.pop(); my.reverse();
+console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
+//solution two
+console.log(my.slice(++zero,counter).reverse()); // ["Elham", "Mazero"]
+// solution three
+console.log(my[2].substring(0,2) + my[1].slice(2)); // "Elzero"
+// solution four
+console.log(my[5].slice(4)+my[1].slice(5).toUpperCase()); // "rO"
