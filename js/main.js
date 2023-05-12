315ce76a43a6530040fe4de1eb2ef7dce2862e35
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-12 18:38:21 +0300

    lessons 48 to 53 => 1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-let zero = 0;
-let counter = 3;
-let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
-//solution one
-my.pop() ; my.pop(); my.reverse();
-console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
-//solution two
-console.log(my.slice(++zero,counter).reverse()); // ["Elham", "Mazero"]
-// solution three
-console.log(my[--counter].substring(zero,counter) + my[--counter].slice(++counter)); // "Elzero"
-// solution four
-console.log(my[(++counter + true)].slice(counter)+my[++zero].slice(++counter).toUpperCase()); // "rO"
+let start = 10;
+let end = 100;
+let exclude = 40;
+
+for (i = start ; i <= end ; i = i + start) {
+  if (i == 40) {
+    continue;
+  }
+    console.log(i);
+}
+
+// Output
+10
+20
+30
+50
+60
+70
+80
+90
+100
