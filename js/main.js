ec8f8b0efcdb61eef0dba9b9016835dd63d8e520
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-12 17:18:03 +0300

    If And Switch Toggle

--- js/main.js
+++ js/main.js
@@ -1,17 +1,55 @@
-let st = "Elzero Web School";
+let job = window.prompt("enter ur role :");
+let holidays = window.prompt("enter ur holidays :");
+let salary = 0;
+let money = 0;
 
-if (st.charAt(st.toLowerCase().indexOf("w")).toLowerCase() === "w") {
-  console.log("Good");
+switch (job) {
+  case "Manager" :
+    salary = 8000;
+    console.log(`Ur salary is : ${salary}`);
+    break;
+  case "IT" :
+    salary = 6000;
+    console.log(`Ur salary is : ${salary}`);
+    break;
+  case "Support" :
+    salary = 6000;
+    console.log(`Ur salary is : ${salary}`);
+    break;
+  case "Developer" :
+    salary = 7000;
+    console.log(`Ur salary is : ${salary}`);
+    break;
+  case "Designer" :
+    salary = 7000;
+    console.log(`Ur salary is : ${salary}`);
+    break;
+  default :
+  salary = 4000;
+  console.log(`Ur salary is : ${salary}`);
 }
 
-if (st.charAt(st.toLowerCase().indexOf("w")).toLowerCase() !== "string") {
-  console.log("Good");
+if (holidays == "0") {
+  money = 5000;
+  console.log(`Ur Money is ${money}`);
 }
-
-if (typeof st.length === "number") {
-  console.log("Good");
+else if (holidays == "2") {
+    money = 3000;
+    console.log(`Ur Money is ${money}`);
 }
-
-if ( st.split(" ")[0].repeat(2) === "ElzeroElzero") {
-  console.log("Good");
+else if (holidays == "3") {
+    money = 2000;
+    console.log(`Ur Money is ${money}`);
+}
+else if (holidays == "4") {
+    money = 1000;
+    console.log(`Ur Money is ${money}`);
+}
+else if (holidays == "5") {
+    money = 0;
+    console.log(`Ur Money is ${money}`);
+}
+else {
+    money = 0;
+    console.log(`Ur Money is ${money}`);
 }
