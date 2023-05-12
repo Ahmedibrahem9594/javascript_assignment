3405c14c525b3ec15d6cc81f8742cd32f8ff0869
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-12 18:54:29 +0300

    lessons 48 to 53 => 2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 let start = 10;
-let end = 100;
-let exclude = 40;
-
-for (i = start ; i <= end ; i = i + start) {
-  if (i == 40) {
-    continue;
+let end = 0;
+let stop = 3;
+for ( i = start ; i >= stop ; i-- ) {
+  if (i < 10) {
+    console.log(`0${i}`);
   }
+  else {
     console.log(i);
+  }
 }
 
 // Output
 10
-20
-30
-50
-60
-70
-80
-90
-100
+09
+08
+07
+06
+05
+04
+03
