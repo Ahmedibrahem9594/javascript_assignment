1c603f1a40a1a7b158ab584807de9c3d05021a98
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-22 20:03:40 +0300

    lessons 54 => 56 / 1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-let start = 0;
-let mix = [1, 2, 3, "A", "B", "C", 4];
+let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let index = 0;
+let counter = 0;
 
-// // Output
-// 2
-// 3
-// 4
-for (let i = ++start; i < mix.length; i++) {
-    if (Number.isInteger(mix[i])) {
-        console.log(`${mix[i]}`);
-    }
+for(let i = index; i < friends.length; i++) {
+        if (Number.isInteger(friends[i]) || friends[i].charAt(0) === "A") {
+                continue;
+        } else {
+                counter++;
+                console.log(`${counter} => ${friends[i]}`);
+        }
 }
+while (friends[index]) {
+        index++;
+        if (friends[index].toString().startsWith("S")) {
+                counter++;
+                console.log(`${counter} ==> ${friends[index]}`);
+        } else if (friends[index].toString().startsWith("M")) {
+                console.log(`${counter} ==> ${friends[index]}`);
+                break;
+        }
+}
+// Output
+// "1 => Sayed"
+// "2 => Mahmoud"
