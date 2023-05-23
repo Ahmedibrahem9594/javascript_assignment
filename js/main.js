b8155673b4138f8a25df857a8f027bc5c9cca944
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-23 10:41:43 +0300

    lessons 57 => 63 /2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-function sayHello(theName, theGender = "none") {
-        if (theGender === "Male") {
-                console.log(`Hello Mr ${theName}`);
-        } else if (theGender === "Female" ) {
-                console.log(`Hello Miss ${theName}`);
+function calculate(firstNum, secondNum = 0, operation) {
+        if (secondNum === 0) {
+                console.log("Second Number Not Found")
         } else {
-                console.log(`Hello ${theName}`);
-        }
+                if (operation === "add") {
+                        console.log(firstNum + secondNum);
+                } else if (operation === "subtract") {
+                        console.log(firstNum - secondNum);
+                } else if (operation === "multiply") {
+                        console.log(firstNum * secondNum);
+                } else {
+                        console.log(firstNum + secondNum);
+                }
+        }  
 }
-
 // Needed Output
-sayHello("Osama", "Male"); // "Hello Mr Osama"
-sayHello("Eman", "Female"); // "Hello Miss Eman"
-sayHello("Sameh"); // "Hello Sameh"
+calculate(20); // Second Number Not Found
+calculate(20, 30); // 50
+calculate(20, 30, 'add'); // 50
+calculate(20, 30, 'subtract'); // -10
+calculate(20, 30, 'multiply'); // 600
