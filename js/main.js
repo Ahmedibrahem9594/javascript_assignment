cbb2fc5bd55e698843c13efb7c6ec976c1917999
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-23 10:36:05 +0300

    lessons 57 => 63 /1

--- js/main.js
+++ js/main.js
@@ -1,25 +1,14 @@
-let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
-let index = 0;
-let counter = 0;
-
-for(let i = index; i < friends.length; i++) {
-        if (Number.isInteger(friends[i]) || friends[i].charAt(0) === "A") {
-                continue;
+function sayHello(theName, theGender = "none") {
+        if (theGender === "Male") {
+                console.log(`Hello Mr ${theName}`);
+        } else if (theGender === "Female" ) {
+                console.log(`Hello Miss ${theName}`);
         } else {
-                counter++;
-                console.log(`${counter} => ${friends[i]}`);
+                console.log(`Hello ${theName}`);
         }
 }
-while (friends[index]) {
-        index++;
-        if (friends[index].toString().startsWith("S")) {
-                counter++;
-                console.log(`${counter} ==> ${friends[index]}`);
-        } else if (friends[index].toString().startsWith("M")) {
-                console.log(`${counter} ==> ${friends[index]}`);
-                break;
-        }
-}
-// Output
-// "1 => Sayed"
-// "2 => Mahmoud"
+
+// Needed Output
+sayHello("Osama", "Male"); // "Hello Mr Osama"
+sayHello("Eman", "Female"); // "Hello Miss Eman"
+sayHello("Sameh"); // "Hello Sameh"
