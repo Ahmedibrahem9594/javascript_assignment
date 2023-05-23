277e5b44ddf715979d8edabec079c4d8493daeb3
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-23 10:48:16 +0300

    lessons 57 => 63 /3

--- js/main.js
+++ js/main.js
@@ -1,21 +1,13 @@
-function calculate(firstNum, secondNum = 0, operation) {
-        if (secondNum === 0) {
-                console.log("Second Number Not Found")
+function ageInTime(theAge) {
+        if (theAge > 100 || theAge < 10) {
+                console.log("Age Out Of Range");
         } else {
-                if (operation === "add") {
-                        console.log(firstNum + secondNum);
-                } else if (operation === "subtract") {
-                        console.log(firstNum - secondNum);
-                } else if (operation === "multiply") {
-                        console.log(firstNum * secondNum);
-                } else {
-                        console.log(firstNum + secondNum);
-                }
-        }  
+                console.log("Your Age In Months " + theAge * 12);
+                console.log("Your Age In Weeks " + theAge * 12 * 4);
+                console.log("Your Age In Days " + theAge * 12 * 4 * 7);
+                console.log("Your Age In Hours " + theAge * 12 * 4 * 7 * 24);
+        }
 }
-// Needed Output
-calculate(20); // Second Number Not Found
-calculate(20, 30); // 50
-calculate(20, 30, 'add'); // 50
-calculate(20, 30, 'subtract'); // -10
-calculate(20, 30, 'multiply'); // 600
+      // Needed Output
+      ageInTime(110); // Age Out Of Range
+      ageInTime(38); // Months Example => 456 Months
