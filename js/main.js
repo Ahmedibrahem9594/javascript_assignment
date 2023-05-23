4b7ace7beb462c8c769041e38bbd25cfd86e419d
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-23 11:13:30 +0300

    lessons 57 => 63 /4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
-function ageInTime(theAge) {
-        if (theAge > 100 || theAge < 10) {
-                console.log("Age Out Of Range");
+function checkStatus(a, b, c) {
+        if (typeof a === "string" && typeof b === "number" && c === true) {
+                console.log(`Hello ${a}, Your Age Is ${b}, You Are Availabe For Hire`);
+        } else if (typeof a === "string" && typeof b === "number" && c === false) {
+                console.log(`Hello ${a}, Your Age Is ${b}, You Are Not Availabe For Hire`);
+        } else if (typeof a === "number" && typeof b === "string" && c === true) {
+                console.log(`Hello ${b}, Your Age Is ${a}, You Are Availabe For Hire`);
+        } else if (typeof a === "number" && typeof b === "string" && c === false) {
+                console.log(`Hello ${b}, Your Age Is ${a}, You Are Not Availabe For Hire`);
+        } else if (a === true && typeof b === "number" && typeof c === "string") {
+                console.log(`Hello ${c}, Your Age Is ${b}, You Are Availabe For Hire`);
+        } else if ( a === false && typeof b === "number" && typeof c === "string") {
+                console.log(`Hello ${c}, Your Age Is ${b}, You Are Not Availabe For Hire`);
+        } else if ( a === true && typeof b === "string" && typeof c === "number") {
+                console.log(`Hello ${b}, Your Age Is ${c}, You Are Not Availabe For Hire`);
+        } else if ( a === false && typeof b === "string" && typeof c === "number") {
+                console.log(`Hello ${b}, Your Age Is ${c}, You Are Not Availabe For Hire`);
         } else {
-                console.log("Your Age In Months " + theAge * 12);
-                console.log("Your Age In Weeks " + theAge * 12 * 4);
-                console.log("Your Age In Days " + theAge * 12 * 4 * 7);
-                console.log("Your Age In Hours " + theAge * 12 * 4 * 7 * 24);
+                console.log("aaa")
         }
 }
       // Needed Output
-      ageInTime(110); // Age Out Of Range
-      ageInTime(38); // Months Example => 456 Months
+      checkStatus("Osama", 50, true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+      checkStatus(60, "Osama", true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+      checkStatus(true, 40, "Osama"); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+      checkStatus(false, "Osama", 42); // "Hello Osama, Your Age Is 38, You Are Not Available For Hire"
