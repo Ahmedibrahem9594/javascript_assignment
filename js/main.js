701c4b2c5c36083b2e29824722a7c88f6781690f
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-27 14:49:53 +0300

    lessons 71 => 78/1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-let checker = (zName) => {
-  return  (status) => {
-    return  (salary) =>  {
-      return status === "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Avaialble`;
-    };
-  };
-}
-
-console.log(checker("Osama")("Available")(4000)); // Osama, My Salary Is 4000
-console.log(checker("Ahmed")("Not Available")()); // Iam Not Avaialble
+let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
+let newMix = mix.map(function (ele) {
+    return isNaN(parseInt(ele)) ? ele : "";
+}).reduce(function (ac,cur) {
+    return `${ac}${cur}`
+})
+console.log(newMix);
+// Elzero
