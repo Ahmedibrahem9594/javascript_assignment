c2f01c5f182964ba0b8ee9cdfce0853005443158
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-05-27 20:42:42 +0300

    lessons 79 => 85 /1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-let nums = [2, 12, 11, 5, 10, 1, 99];
-let newNums = nums.reduce(function (acc, curr) {
-    return curr % 2 === 0 ? curr * acc : curr + acc ;
-},1)
-console.log(newNums);
-// 500
+// Create Your Object Here
+let member = {
+    name : "Elzero",
+    age : 38,
+    country : "Egypt",
+    fullDetails : function () {
+        return `My name is Elzero  , my age is 38, i live in egypt`;
+    },
+};
+
+console.log(member.name); // Elzero
+console.log(member.age); // 38
+console.log(member.country); // Egypt
+console.log(member.fullDetails());
+// My Name Is Elzero, My Age Is 38, I Live in Egypt
