f52289994b485f2b0ff370bf332ed4d622dbdba3
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-10 15:02:47 +0300

    86 => 93 /5

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-const divOne = document.querySelector(".one");
-const divTwo = document.querySelector(".two");
-const divOneAttr = divOne.getAttribute("title");
-const divTwoAttr = divTwo.getAttribute("title");
-const divOneText = divOne.innerHTML
-const divTwoText = divTwo.innerHTML
-//change attr 
-divOne.setAttribute("title",divTwoAttr);
-divTwo.setAttribute("title",divOneAttr);
-//change inner html
-divOne.innerHTML = divTwoText
-divTwo.innerHTML = `${divOneText} ${document.getElementsByTagName("div").length}`
+const myImg = document.querySelectorAll("img");
+
+myImg.forEach((e) => e.getAttribute("alt") === null ? e.setAttribute("alt","Elzero New") :  e.setAttribute("alt","old"))
