c1efa75cd416556aa07a150a165d09c869229997
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-10 14:11:17 +0300

    86 => 93 /2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
-// let myDiv = document.getElementById("elzero");
-// let myDiv = document.getElementsByName("js");
-// let myDiv = document.getElementsByClassName("element");
-// let myDiv = document.getElementsByTagName("div");
-// let myDiv = document.querySelector("#elzero");
-// let myDiv = document.querySelector(".element");
-// let myDiv = document.querySelector("div");
-// let myDiv = document.querySelectorAll("div");
-// let myDiv = document.querySelectorAll("#elzero");
-// let myDiv = document.querySelectorAll(".element");
-// let myDiv = document.body.firstElementChild
-// let myDiv = document.body.lastElementChild.previousElementSibling
-// let myDiv = document.body.firstChild.nextSibling
-// let myDiv = document.body.firstChild.nextSibling
-// let myDiv = document.querySelector("[name = js]");
-// let myDiv = document.querySelectorAll("[name = js]");
-console.log(myDiv[0]);
+let myImg = document.querySelectorAll("img");
+myImg.forEach(function(e) {
+    e.setAttribute("src","https://elzero.org/wp-content/themes/elzero/imgs/logo.png");
+    e.setAttribute("alt","Elzero Logo");
+})
