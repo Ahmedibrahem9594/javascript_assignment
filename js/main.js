022877bcde36cf3041bae067cfbf747dc312a97b
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-10 21:31:42 +0300

    115=>122/1

--- js/main.js
+++ js/main.js
@@ -1,37 +1,5 @@
-const fontSelectors = document.querySelector("#gov");
-const inputName = document.querySelector("#text-Name");
-const inputPhone = document.querySelector("#text-Phone");
-const inputDegree = document.querySelector("#text-Degree");
-let optionEl = document.querySelectorAll("option");
-const selectGov = document.querySelector("select");
-//
-//to uppercase for all content
-optionEl.forEach((e)=> e.innerHTML = e.value.toUpperCase())
+let myNumbers = [1, 2, 3, 4, 5];
 
-
-//condition to check local storage
-if (localStorage.getItem("Name") || localStorage.getItem("Phone") || localStorage.getItem("Degree") || localStorage.getItem("Gov")  ) {
-    inputName.value = localStorage.getItem("Name") ;
-    inputPhone.value = localStorage.getItem("Phone");
-    inputDegree.value = localStorage.getItem("Degree");
-    selectGov.value = localStorage.getItem("Gov");
-}
-//event on change
-inputName.addEventListener("change",(e) => {
-    const nameLocal =  e.target.value;
-    localStorage.setItem("Name",nameLocal);
-})
-inputPhone.addEventListener("change",(e) => {
-    const phoneLocal =  e.target.value;
-    localStorage.setItem("Phone",phoneLocal);
-})
-inputDegree.addEventListener("change",(e) => {
-    const degreeLocal =  e.target.value;
-    localStorage.setItem("Degree",degreeLocal);
-})
-selectGov.addEventListener("change",(e) => {
-    const govLocal =  e.target.value;
-    localStorage.setItem("Gov",govLocal);
-})
-
-window.onload = () => localStorage.clear()
+// Write Your Destructuring Assignment Here
+let [a, b, c, d, e] = myNumbers;
+console.log(a * e); // 5
