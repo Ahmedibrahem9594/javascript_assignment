f026be6bb53f3aa58922de03c2c59162d6cf42a4
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-10 21:22:40 +0300

    111 => 114/2

--- js/main.js
+++ js/main.js
@@ -1,43 +1,37 @@
-// font-family: 'Cairo', sans-serif;
-// font-family: 'Open Sans', sans-serif;
-// font-family: 'Roboto', sans-serif;
-
-const fontSelectors = document.querySelector("#font-select");
-const colorSelectors = document.querySelector("#color");
-const fontSizeSelectors = document.querySelector("#font-size");
-const inputForm = document.querySelector("#text-input");
+const fontSelectors = document.querySelector("#gov");
+const inputName = document.querySelector("#text-Name");
+const inputPhone = document.querySelector("#text-Phone");
+const inputDegree = document.querySelector("#text-Degree");
 let optionEl = document.querySelectorAll("option");
-
+const selectGov = document.querySelector("select");
 //
 //to uppercase for all content
 optionEl.forEach((e)=> e.innerHTML = e.value.toUpperCase())
 
 
 //condition to check local storage
-if(window.localStorage.getItem("fontSizeLocal") || window.localStorage.setItem("fontFamilyLocal",fontFamilyLocal) || window.localStorage.setItem("colorLocal",colorLocal) ) {
-    inputForm.style.fontSize = window.localStorage.getItem("fontSizeLocal");
-    inputForm.style.fontFamily = window.localStorage.getItem("fontFamilyLocal");  
-    inputForm.style.backgroundColor = window.localStorage.getItem("colorLocal");
+if (localStorage.getItem("Name") || localStorage.getItem("Phone") || localStorage.getItem("Degree") || localStorage.getItem("Gov")  ) {
+    inputName.value = localStorage.getItem("Name") ;
+    inputPhone.value = localStorage.getItem("Phone");
+    inputDegree.value = localStorage.getItem("Degree");
+    selectGov.value = localStorage.getItem("Gov");
 }
-
 //event on change
-fontSizeSelectors.addEventListener("change",(e) => {
-    const fontSizeLocal = e.target.value + "px" ;
-    window.localStorage.setItem("fontSizeLocal",fontSizeLocal);
-    inputForm.style.fontSize = window.localStorage.getItem("fontSizeLocal");  
-}) 
-
-//event on change
-fontSelectors.addEventListener("change",(e) => {
-    const fontFamilyLocal  = e.target.value;
-    window.localStorage.setItem("fontFamilyLocal",fontFamilyLocal);
-    inputForm.style.fontFamily = window.localStorage.getItem("fontFamilyLocal");  
+inputName.addEventListener("change",(e) => {
+    const nameLocal =  e.target.value;
+    localStorage.setItem("Name",nameLocal);
 })
-//event on change
-colorSelectors.addEventListener("change",(e) => {
-    const colorLocal  = e.target.value;
-    window.localStorage.setItem("colorLocal",colorLocal);
-    inputForm.style.backgroundColor = window.localStorage.getItem("colorLocal");  
+inputPhone.addEventListener("change",(e) => {
+    const phoneLocal =  e.target.value;
+    localStorage.setItem("Phone",phoneLocal);
+})
+inputDegree.addEventListener("change",(e) => {
+    const degreeLocal =  e.target.value;
+    localStorage.setItem("Degree",degreeLocal);
+})
+selectGov.addEventListener("change",(e) => {
+    const govLocal =  e.target.value;
+    localStorage.setItem("Gov",govLocal);
 })
 
-
+window.onload = () => localStorage.clear()
