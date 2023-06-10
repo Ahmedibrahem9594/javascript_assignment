b50542a848d990c8709e29eade7c3f4ce9c3d375
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-10 13:05:04 +0300

    102 => 110 / 4

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ let counter = 10;
 let intervalCounter =  setInterval(function() {
     document.body.appendChild(myDivCounter);
     myDivCounter.innerHTML = counter;
-    counter -= 1;
-    counter < 0 ? clearInterval(intervalCounter) : "";
+    counter -= 1; 
+    counter < 0 ? window.open("https://elzero.org/") && clearInterval(intervalCounter) : "";
 },1000);
