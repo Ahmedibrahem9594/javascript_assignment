85f481c18e0776294a3a2599df274ff22938f88a
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-10 21:33:21 +0300

    115=>122/2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-let myNumbers = [1, 2, 3, 4, 5];
+let mySkills = ["HTML", "CSS", "JavaScript", ["PHP", "Python", ["Django", "Laravel"]]];
 
 // Write Your Destructuring Assignment Here
-let [a, b, c, d, e] = myNumbers;
-console.log(a * e); // 5
+let [a, b, c, [d, e, [f, g]]] = mySkills
+console.log(`My Skills: ${a}, ${b}, ${c}, ${d}, ${e}, ${f}, ${g}`);
+
+// My Skills: HTML, CSS, JavaScript, PHP, Python, Django, Laravel
