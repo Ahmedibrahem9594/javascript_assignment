a00d85b83c16a89a90d81e36d7a4ccaa3a7c5705
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-11 20:34:27 +0300

    115=>122/5

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
-const member = {
-    age: 30,
-    working: false,
-    country: "Egypt",
-    hobbies: ["Reading", "Swimming", "Programming"],
+const game = {
+    title: "YS",
+    developer: "Falcom",
+    releases: {
+        "Oath In Felghana": ["USA", "Japan"],
+        "Ark Of Napishtim": {
+        US: "20 USD",
+        JAP: "10 USD",
+    },
+        Origin: "30 USD",
+    },
 };
 
-  // Write Your Destructuring Assignment Here
-let {age:a, working:w, country:c, hobbies:[h1,h2,h3]} = member
+// let {releases:{"Oath In Felghana":a,"Ark Of Napishtim":b,"Origin":v}} = game
+// console.log(releases)
 
-console.log(`My Age Is ${a} And Iam ${w ? "" : "Not"} Working`);
-  // My Age Is 30 And Iam Not Working
 
-console.log(`I Live in ${c}`);
-  // I Live in Egypt
+//   // Write Your Destructuring Assignment/s Here
+[o,a,] = Object.keys(game.releases) 
+let {title:t, developer:d,releases:{"Oath In Felghana":[u,j],"Ark Of Napishtim":{US:u_price,JAP:j_price},"Origin":or}} = game;
 
-console.log(`My Hobbies: ${h1} And ${h3}`);
-  // My Hobbies: Reading And Programming
+console.log(`My Favourite Games Style Is ${t} Style`);
+//   // My Favourite Games Style Is YS Style
+
+console.log(`And I Love ${d} Games`);
+//   // And I Love Falcom Games
+
+console.log(`My Best Release Is ${o} It Released in ${u} & ${j}`);
+//   // My Best Release Is Oath In Felghana It Released in USA & Japan
+
+console.log(`Although I Love ${a}`);
+//   // Although I Love Ark Of Napishtim
+
+console.log(`${a} Price in USA Is ${u_price}`);
+//   // Ark Of Napishtim Price in USA Is 20 USD
+
+console.log(`${a} Price in Japan Is ${j_price}`);
+//   // Ark Of Napishtim Price in Japan Is 10 USD
+
+console.log(`Origin Price Is ${or}`);
+//   // Origin Price Is 30 USD
