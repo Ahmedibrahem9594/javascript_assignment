8830b42814e89a14932a990fbd2ee2c07c962c44
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-12 12:50:42 +0300

    123 => 133 /5

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
-let myInfo = {
-    username: "Osama",
-    role: "Admin",
-    country: "Egypt",
-};
-let newMap = new Map(Object.entries(myInfo));
-console.log(newMap)
-console.log(newMap.size)
-console.log(newMap.has("role"))
- // Needed Output
-// Map(3) {'username' => 'Osama', 'role' => 'Admin', 'country' => 'Egypt'}
-// 3
-// true
+let theName = "Elzero";
+let newSet = [...theName]
+let newSetS = Array.from(theName)
+let newSetT = theName.split("")
+let newSetF = theName.toString().split("") 
+let newSetv = [...new Map(Object.entries( {theName})).values()].toString().split('') 
+// Needed Output
+// ['E', 'l', 'z', 'e', 'r', 'o']
