b9b2a14ef79ebb84c3e5c6bcc3211907966c912b
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-12 11:39:44 +0300

    123 => 133 /3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
-let newSetName = new Set(myFriends);
-console.log((newSetName.size), (Array.from(newSetName).sort()))
-// Needed Output
-// (4) ['Ahmed', 'Mahmoud', 'Osama', 'Sayed']
+let myInfo = {
+    username: "Osama",
+    role: "Admin",
+    country: "Egypt",
+};
+let newMap = new Map(Object.entries(myInfo));
+console.log(newMap)
+console.log(newMap.size)
+console.log(newMap.has("role"))
+ // Needed Output
+// Map(3) {'username' => 'Osama', 'role' => 'Admin', 'country' => 'Egypt'}
+// 3
+// true
