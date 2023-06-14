10dcfc367a3f9e1a4a4afd3111b660176156f483
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-14 21:50:44 +0300

    134=>146/5

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-let re = /https?:\/\/(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/i;
-// https? => it is optional to contains https
-// \/\/(? => it is // and it optional
-// [-\w]+ => it words litters and uncountable
-// * => match zero or more
-// / => it is escaped character
+let date1 = "25/10/1982";
+let date2 = "25 - 10 - 1982";
+let date3 = "25 10 1982";
+let date4 = "25 10 82";
+
+let re = /\d{2}\/?\s?\-?\s?\d{2}\/?\s?\-?\s?\d{2,4}/ig
+
+console.log(date1.match(re)); // "25/10/1982"
+console.log(date2.match(re)); // "25 - 10 - 1982"
+console.log(date3.match(re)); // "25 10 1982"
+console.log(date4.match(re)); // "25 10 82"
