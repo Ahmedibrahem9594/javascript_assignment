231a82848c2227bc694871c5a16c369211480c2b
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-06-14 21:39:43 +0300

    134=>146/3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-let specialNames = "Os10O OsO Os100O Osa100O Os1000 Os100m";
-let regExp = /(os)(\d+)?o/ig;
-console.log(specialNames.match(regExp));
-// Output
-// ['Os10O', 'OsO', 'Os100O']
+let phone = "+(995)-123 (4567)";
+let regExp = /\+\((\d{3})\)-\d{3}\s\(\d{4}\)/ig
+console.log(phone.match(regExp))
