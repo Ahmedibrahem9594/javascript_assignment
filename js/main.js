5cf2a81105a92aa82ce68b3c5e544510e968b87f
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-03 00:36:44 +0300

    lessons 147 => 158 / 2

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
-// function Car(name, model, price) {
-//     this.n = name;
-//     this.m = model;
-//     this.p = price;
-// }
-
-//convert class syntax
-class Car {
-    constructor (name, model, price) {
-        this.n = name;
-        this.m = model;
-        this.p = price;
+class Phone {
+    constructor(name, serial, price) {
+      this.name = name;
+      this.serial = serial;
+      this.price = price;
     }
-    run() {
-        return ("Car Is Running Now");
+  }
+  
+  // Write Tablet Class Here
+class Tablet extends Phone {
+    constructor(name, serial, price, size = "Unknown") {
+        super(name, serial, price);
+        this.size = size;
     }
-    stop() {
-        return (" Car Is Stopped");
-    }
-    info() {
-        return (`Car One Name Is ${this.n} And Model IS ${this.m} And Price IS ${this.p}`);
+    fullDetails() {
+        return `${this.name} serial is ${this.serial} and size is ${this.size}`;
     }
 }
 
-let car1 = new Car("MG","2022","420000");
-console.log(car1.info());
-console.log(car1.run());
-// Needed Output
-//"Car One Name Is MG And Model Is 2022 And Price Is 420000"
-//"Car Is Running Now"
+
+
+  
+  let TabletOne = new Tablet("iPad", 100200300, 1500, 12.9);
+  let TabletTwo = new Tablet("Nokia", 350450650, 800, 10.5);
+  let TabletThree = new Tablet("LG", 250450650, 650);
+  
+  console.log(`${TabletOne.fullDetails()}`);
+  // iPad Serial is 100200300 And Size Is 12.9
+  
+  console.log(`${TabletTwo.fullDetails()}`);
+  // Nokia Serial is 350450650 And Size Is 10.5
+  
+  console.log(`${TabletThree.fullDetails()}`);
+  // LG Serial is 250450650 And Size Is Unknown
