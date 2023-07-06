6721a8c780135149fa8ab53354f58f1224c93ffb
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-07 01:13:40 +0300

    lessons 147 => 158 / 5

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
-//Write code here
-String.prototype.addlove = function () {
-  return `I Love Elzero Web School`;
-}
-//Do not edit below
-let mystr = "elzero";
-console.log(mystr.addlove()); // i love elzero web school
+const myObj = {
+  username: "elzero",
+  id: 100,
+  score: 1000,
+  country: "Egypt",
+};
+
+//Code
+
+// configurable: true
+// enumerable: true
+// value: "elzero"
+// writable: true
+
+Object.defineProperty(myObj, "id", {
+enumerable: false,
+});
+
+Object.defineProperty(myObj, "country", {
+  enumerable: false,
+  writable:false,
+  configurable:true,
+});
+
+Object.defineProperty(myObj, "score", {
+  configurable: false,
+});
+
+delete myObj.country;
+
+
+myObj.score = 500;
+
+for (let prop in myObj) {
+  console.log(`${prop} => ${myObj[prop]}`);
+};
+
+console.log(myObj);
+
+//needed output
+
+// "username => elzero"
+// "score => 1000"
+// {username: "elzero", score:1000, id:100}
