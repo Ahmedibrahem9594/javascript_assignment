9efba94c74e569254a32830fae6ad2e4368655bd
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-08 01:38:22 +0300

    159 =>  168 /6

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
-
-console.log(performance.mark(0))
-
-for (let i =0; i <= 1000 ; i++) {
-    console.log(i);
+// Write Your Generator Function Here
+function* gen() {
+    let index = 0;
+    while(true) {
+        yield index ++;
+    }
 }
 
-console.log(`Loop Took ${performance.now()} Milliseconds`)
 
+let generator = gen();
 
-
-
-// Needed Output
-
-"Loop Took 1921 Milliseconds."
+console.log(generator.next()); // {value: 14, done: false}
+console.log(generator.next()); // {value: 154, done: false}
+console.log(generator.next()); // {value: 494, done: false}
+console.log(generator.next()); // {value: 1034, done: false}
+console.log(generator.next()); // {value: 1774, done: false}
+console.log(generator.next()); // {value: 2714, done: false}
+console.log(generator.next()); // {value: 3854, done: false}
+console.log(generator.next()); // {value: 5194, done: false}
+console.log(generator.next()); // {value: 6734, done: false}
