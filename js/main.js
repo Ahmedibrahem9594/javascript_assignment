8a4173152027f728d83565a73a0428b1aed120d1
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-08 01:15:54 +0300

    159 =>  168 / 3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-let newDate = new Date("1-1-1980");
-console.log(new Date(newDate.setSeconds(1)));
+let newDate = new Date();
+
+
+console.log(new Date(newDate.setDate(-30)));
+console.log(`Previous Month Is April And Last Day IS ${new Date(newDate.setDate(-30)).getDate()}`)
 
 
 // Needed Output
+"Sat Apr 30 2022 18:13:20 GMT+0200 (Eastern European Standard Time)"
+"Previous Month Is April And Last Day Is 30"
 
-"Tue Jan 01 1980 00:00:01 GMT+0200 (Eastern European Standard Time)"
