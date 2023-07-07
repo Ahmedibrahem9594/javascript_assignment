19e3f69f500dc91caa2dd2b2ad072ce10f5c27f3
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-08 02:31:26 +0300

    159 => 168 / 8

--- js/main.js
+++ js/main.js
@@ -1,31 +1,8 @@
-function* genNumbers() {
-    yield* [1, 2, 2, 2, 3, 4, 5];
-  }
-  function* genLetters() {
-    yield* ["A", "B", "B", "B", "C", "D"];
-  }
-  
-  // Write Your Generator Function Here
-  function newset(en) {
-    return new Set(en)
-  }
+import { modOne } from "./mod-two.js";
+import { calc } from "./mod-one.js";
 
- let newLett =  newset(genLetters());
- let newNum = newset(genNumbers());
+// main.js File
 
-  function* genAll() {
-    yield* newNum;
-    yield* newLett;
-  }
-  
-  let generator = genAll();
-  
-  console.log(generator.next()); // {value: 1, done: false}
-  console.log(generator.next()); // {value: 2, done: false}
-  console.log(generator.next()); // {value: 3, done: false}
-  console.log(generator.next()); // {value: 4, done: false}
-  console.log(generator.next()); // {value: 5, done: false}
-  console.log(generator.next()); // {value: "A", done: false}
-  console.log(generator.next()); // {value: "B", done: false}
-  console.log(generator.next()); // {value: "C", done: false}
-  console.log(generator.next()); // {value: "D", done: false}
+console.log(calc(modOne.numOne, modOne.numTwo, modOne.numThree)); // 60
+
+// mod-two.js File
---
+++ js/mod-two.js
@@ -0,0 +1,9 @@
+let a = 10; // Do Not Edit Names
+let b = 20; // Do Not Edit Names
+let c = 30; // Do Not Edit Names
+
+export let modOne = new Object({
+    numOne: a,
+    numTwo: b,
+    numThree: c,
+})
