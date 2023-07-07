19bdb01d1e80bf8b62fa340c9079c9fdd407b3a8
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-08 01:27:45 +0300

    159 =>  168 /5

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-console.log(new Date("10-25-1982"));
-console.log(new Date(1982,9,25));
-let lastDate = new Date();
-lastDate.setDate(25);
-lastDate.setMonth(9);
-lastDate.setFullYear(1982)
-console.log(lastDate);
+
+console.log(performance.mark(0))
+
+for (let i =0; i <= 1000 ; i++) {
+    console.log(i);
+}
+
+console.log(`Loop Took ${performance.now()} Milliseconds`)
+
+
 
 
 // Needed Output
 
-"Mon Oct 25 1982 00:00:00 GMT+0200 (Eastern European Standard Time)"
-"Mon Oct 25 1982 00:00:00 GMT+0200 (Eastern European Standard Time)"
-"Mon Oct 25 1982 00:00:00 GMT+0200 (Eastern European Standard Time)"
+"Loop Took 1921 Milliseconds."
