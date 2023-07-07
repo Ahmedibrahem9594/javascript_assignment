f6d5d01e853533f0171d7ef79e150135dd12c628
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-08 01:09:29 +0300

    159 =>  168 / 2

--- js/main.js
+++ js/main.js
@@ -1,29 +1,7 @@
-let birthDate = new Date("5-9-1994");
-let currentDate = new Date();
-
-let diff = currentDate.getTime() - birthDate.getTime();
-
-let ageSeconds =  diff/1000;
-console.log(ageSeconds, "Seconds");
-console.log("#".repeat(50));
-let ageMinutes =  ageSeconds/60;
-console.log(ageMinutes, "Minutes");
-console.log("#".repeat(50));
-let ageDays =  ageMinutes/60;
-console.log(ageDays, "Days");
-console.log("#".repeat(50));
-let ageMonths =  ageDays/24;
-console.log(ageMonths, "Months");
-console.log("#".repeat(50));
-let ageYears = ageMonths/365;
-console.log(ageYears, "Years");
-
+let newDate = new Date("1-1-1980");
+console.log(new Date(newDate.setSeconds(1)));
 
 
 // Needed Output
-"1247939400 Seconds"
-"20798990 Minutes"
-"346650 Hours"
-"14444 Days"
-"481 Months"
-"40 Years"
+
+"Tue Jan 01 1980 00:00:01 GMT+0200 (Eastern European Standard Time)"
