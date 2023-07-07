4f762a053b566721854d1c2197e0eba337576e0e
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-08 01:45:47 +0300

    159 =>  168 /7

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
-// Write Your Generator Function Here
-function* gen() {
-    let index = 0;
-    while(true) {
-        yield index ++;
-    }
-}
+function* genNumbers() {
+    yield* [1, 2, 2, 2, 3, 4, 5];
+  }
+  function* genLetters() {
+    yield* ["A", "B", "B", "B", "C", "D"];
+  }
+  
+  // Write Your Generator Function Here
+  function newset(en) {
+    return new Set(en)
+  }
 
+ let newLett =  newset(genLetters());
+ let newNum = newset(genNumbers());
 
-let generator = gen();
-
-console.log(generator.next()); // {value: 14, done: false}
-console.log(generator.next()); // {value: 154, done: false}
-console.log(generator.next()); // {value: 494, done: false}
-console.log(generator.next()); // {value: 1034, done: false}
-console.log(generator.next()); // {value: 1774, done: false}
-console.log(generator.next()); // {value: 2714, done: false}
-console.log(generator.next()); // {value: 3854, done: false}
-console.log(generator.next()); // {value: 5194, done: false}
-console.log(generator.next()); // {value: 6734, done: false}
+  function* genAll() {
+    yield* newNum;
+    yield* newLett;
+  }
+  
+  let generator = genAll();
+  
+  console.log(generator.next()); // {value: 1, done: false}
+  console.log(generator.next()); // {value: 2, done: false}
+  console.log(generator.next()); // {value: 3, done: false}
+  console.log(generator.next()); // {value: 4, done: false}
+  console.log(generator.next()); // {value: 5, done: false}
+  console.log(generator.next()); // {value: "A", done: false}
+  console.log(generator.next()); // {value: "B", done: false}
+  console.log(generator.next()); // {value: "C", done: false}
+  console.log(generator.next()); // {value: "D", done: false}
