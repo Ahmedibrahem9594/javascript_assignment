9efd6e3cf08a6f41603fcf8975ed714b4269ed82
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-16 21:23:42 +0300

    169 => 178 / 1

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import { modOne } from "./mod-two.js";
-import { calc } from "./mod-one.js";
-
-// main.js File
-
-console.log(calc(modOne.numOne, modOne.numTwo, modOne.numThree)); // 60
-
-// mod-two.js File
