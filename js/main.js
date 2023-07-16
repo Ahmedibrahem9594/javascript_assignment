f22fe5f214a9bf64aa828c9c9535359cc69fb56e
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-16 22:29:25 +0300

    179 => 188 / 1

--- js/main.js
+++ js/main.js
@@ -1,30 +1,18 @@
-let getData = () => {
-    const requests = new XMLHttpRequest();
-    console.log(requests.readyState);
-    requests.open("GET", "/articles.json");
-    requests.send();
-    requests.onreadystatechange = () => {
-        if (requests.readyState === 4 && requests.status === 200) {
-            let data = requests.responseText;
-            let jsData =  JSON.parse(data);
-            for (let prop in jsData) {
-                let textData ="";
-                let mainDiv = document.createElement("div");
-                mainDiv.className = "data";
-                let sectionDiv = document.createElement("div");
-                mainDiv.appendChild(sectionDiv);
-                textData += `       <h2>${jsData[prop].article_title}</h2>
-                                    <p>${jsData[prop].article_content}</p>
-                                    <p>${jsData[prop].author.toUpperCase()}</p>
-                                    <p>${jsData[prop].section.toUpperCase()}</p>`
-                sectionDiv.innerHTML = textData;
-                document.body.appendChild(mainDiv);
-            }
-            console.log(jsData);
-            
-        
-        }   
-    }
-}
+let myFetchData = fetch("/articles.json");
 
-getData()
+myFetchData.then((resovled)=> {
+    console.log(resovled);
+    return resovled.json();
+}).then((data)=> {
+    for(let i =0; i<= 5; i++) {
+        let textData ="";
+        let mainDiv = document.createElement("div");
+        mainDiv.className = "data";
+        let sectionDiv = document.createElement("div");
+        mainDiv.appendChild(sectionDiv);
+        textData += `       <h3>${data[i].title}</h3>
+                            <p>${data[i].description}</p>`
+        sectionDiv.innerHTML = textData;
+        document.body.appendChild(mainDiv);
+    } 
+})
