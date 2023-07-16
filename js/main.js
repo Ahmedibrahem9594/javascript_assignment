177c543ce0181ef63c7878e2d6838e04ab30d979
ahmedibrahem9594 <ahmedibrahem9594@gmail.com>
2023-07-16 22:15:48 +0300

    169 => 178 / 4

--- js/main.js
+++ js/main.js
@@ -8,10 +8,21 @@ let getData = () => {
             let data = requests.responseText;
             let jsData =  JSON.parse(data);
             for (let prop in jsData) {
-                jsData[prop].section = "all";
+                let textData ="";
+                let mainDiv = document.createElement("div");
+                mainDiv.className = "data";
+                let sectionDiv = document.createElement("div");
+                mainDiv.appendChild(sectionDiv);
+                textData += `       <h2>${jsData[prop].article_title}</h2>
+                                    <p>${jsData[prop].article_content}</p>
+                                    <p>${jsData[prop].author.toUpperCase()}</p>
+                                    <p>${jsData[prop].section.toUpperCase()}</p>`
+                sectionDiv.innerHTML = textData;
+                document.body.appendChild(mainDiv);
             }
-            console.log(jsData)
-            JSON.stringify(jsData);
+            console.log(jsData);
+            
+        
         }   
     }
 }
